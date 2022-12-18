58e1bb6831a9ab8c1b62b3760cb86dcfb01bf077
Stenstromen <peppoji@gmail.com>
2022-12-18 23:52:19 +0100

    WIP

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -28,13 +28,43 @@ function Dashboard() {
         setServers(res.data.servers.sort());
         // https://www.tutorialspoint.com/counting-unique-elements-in-an-array-in-javascript
         const countUnique = (arr: any) => {
+          const arrCounts: any = [];
           const counts: any = {};
           for (let i = 0; i < arr.length; i++) {
-            counts[arr[i].currentMap] = 1 + (counts[arr[i].currentMap] || 0);
+            //const counts: any = {};
+            counts[arr[i].currentMap] = 1 + (counts[arr[i].currentMap] || 0); 
+            arrCounts.push(counts);
           }
-          return counts;
+          //arrCounts.push(counts);
+          return arrCounts;
         };
-        console.log(countUnique(res.data.servers));
+        const unique = (arr: any) => {
+          const arrCounts: any = [];
+          arr.forEach((element: { currentMap: any; }) => {
+            arrCounts.push(element.currentMap)
+          });
+
+          const count = arrCounts.reduce((accumulator: { [x: string]: any; }, value: string | number) => {
+            return {...accumulator, [value]: (accumulator[value] || 0) + 1};
+          }, {});
+
+          return count;
+        }
+
+        console.log(unique(res.data.servers));
+        /* const countUniques = (orders = []) => {
+          const tableObj:any = {}, //foodObj = {};
+          orders.forEach: any((el: { table_id: string | number; }) => {
+             tableObj[el.table_id] = null;
+             //foodObj[el.food_id] = null;
+          });
+          const tableUniqueIDs = Object.keys(tableObj).length;
+          //const foodUniqueIDs = Object.keys(foodObj).length;
+          return {
+             tableUniqueIDs//, foodUniqueIDs
+          };
+       }; */
+       //console.log(countUniques(res.data.servers));
       })
       .catch((err) => {
         console.log(err);
